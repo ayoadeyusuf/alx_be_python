fee17b8bb6a5e6340f75028f0d82081f6e55da4f
Ayoade Yusuf <ayoadeyusuf98@gmail.com>
2025-07-12 22:48:37 +0300

    Create basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+
+# operations
+sum = number1 + number2
+diff = number1 - number2
+product = number1 * number2
+
+# output
+print('Addition of {number1} and {number2} is {sum}')
+print('Subtraction of {number1} and {number2} is {diff}')
+print('Multiplication of {number1} and {number2} is {prod}')
