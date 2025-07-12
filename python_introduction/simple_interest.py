1f70171cbc905e526aa1dd77dcefde3c663eca61
Ayoade Yusuf <ayoadeyusuf98@gmail.com>
2025-07-12 22:56:23 +0300

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -4,7 +4,7 @@ rate = 0.05
 time = 3
 
 # calculating interest
-interest = (principal * rate * time) / 100
+interest = principal * rate * time
 
 # output
 print('The simple interest is: {interest}')
