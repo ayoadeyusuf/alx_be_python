95dd270fee80c31961e145839a5fffb46bdbef8f
Ayoade Yusuf <ayoadeyusuf98@gmail.com>
2025-07-12 22:53:42 +0300

    Create simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# variable declaration and assignment
+principal = 1000
+rate = 0.05
+time = 3
+
+# calculating interest
+interest = (principal * rate * time) / 100
+
+# output
+print('The simple interest is: {interest}')
