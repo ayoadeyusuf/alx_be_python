18051926de007f302f9b81a8f2a55503b7c6d75a
Ayoade Yusuf <ayoadeyusuf98@gmail.com>
2025-07-26 12:20:38 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,6 +9,9 @@ def perform_operation(num1, num2, operation):
     case 'divide':
       if num2 == 0:
         print('Error!!! Division by zero')
+        while num2 == 0:
+          num2 = float(input("Enter a number other than zero: "))
       elif num2 != 0:
-        return (num1 / num2)
+        num2 = num2
+      return (num1 / num2)
   
