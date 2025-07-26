0892d30f2443ddde7f6db771ece1b7ca1f1f90e1
Ayoade Yusuf <ayoadeyusuf98@gmail.com>
2025-07-26 12:06:13 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,6 +9,6 @@ def perform_operation(num1, num2, operation):
     case 'divide':
       if num2 == 0:
         print('Error!!! Division by zero')
-      else:
+      elif num2 != 0:
         return (num1 / num2)
   
