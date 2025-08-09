c4a1ec98f4e5a80e995a3eb6bd81489f21b355b8
Ayoade Yusuf <ayoadeyusuf98@gmail.com>
2025-08-09 03:42:58 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,13 +1,13 @@
 class BankAccount:
     def __init__(self, account_balance):
         self.account_balance = account_balance
-
+  
     def deposit(self,amount):
-        account_balance = self.account_balance + amount
+        account_balance = float(self.account_balance + amount)
         return account_balance
 
     def withdraw(self,amount):
-        account_balance = self.account_balance - amount
+        account_balance = float(self.account_balance - amount)
         return account_balance
 
     def display_balance(self):
